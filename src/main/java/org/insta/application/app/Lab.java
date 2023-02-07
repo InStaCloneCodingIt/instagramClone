8cd9b4ace9506a5106b7d4a4e8b6e391ddc721e7
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-02-07 22:23:51 +0900

    commit Test

--- src/main/java/org/insta/application/app/Lab.java
+++ src/main/java/org/insta/application/app/Lab.java
@@ -1,7 +1,12 @@
 package org.insta.application.app;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class Lab {
@@ -9,6 +14,12 @@ public class Lab {
     @GetMapping("/test")
     public String test() {
         return "Hello World";
+    }
+    @GetMapping("/api/v1/test")
+    public ResponseEntity<Map<String,Object>> testRest() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("Commit","Test");
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
 }
