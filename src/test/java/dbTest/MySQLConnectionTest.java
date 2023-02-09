00a431da6fb90ae8f97a74053a81ecf88d96f7e8
jeonghyun <jhyeon310@naver.com>
2023-02-09 16:49:57 +0900

    0209

--- src/test/java/dbTest/MySQLConnectionTest.java
+++ src/test/java/dbTest/MySQLConnectionTest.java
@@ -1,5 +1,7 @@
 package dbTest;
 
+import org.junit.Test;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -8,11 +10,11 @@ public class MySQLConnectionTest {
     // MySQL Connector의 클래스, DB 연결 드라이버 정의
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     // DB 경로
-    private static final String URL = "jdbc:mysql://localhost:3306/데이터베이스?" +
-                                    "serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String URL = "jdbc:mysql://localhost:3306/데이터베이스?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
     private static final String USER = "testUser";
     private static final String PASSWORD = "user1234";
 
+    @Test
     public void testConnection() throws Exception{
         Class.forName(DRIVER);
         try{
